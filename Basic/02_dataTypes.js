28e44ad32b4d599dc396516cec7341e9bbcb1fcb
Tanish <108221758+Tanish-07@users.noreply.github.com>
2024-09-11 01:18:51 +0000

    02_DataTypes

---
+++ Basic/02_dataTypes.js
@@ -0,0 +1,18 @@
+//      DataTypes
+
+//Primitive DataTypes
+
+// number => 2 ^ 53
+// bigint
+// string => " "
+// boolean => true/false
+// null => stand alone value   // But its typeof gives output of object
+// undefined => undefined
+// symbol => Utilised to use unique items 
+
+//Non-Primitive DataTypes
+
+// object
+
+console.log(typeof null);       // object
+console.log(typeof undefined);  // undefined
